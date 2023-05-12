ead5475ab205a14b811c0b39a6954a2d8803c27b
rsefaq <78513815+rsefaq@users.noreply.github.com>
2023-05-12 09:04:25 +0900

    Create 05.js

---
+++ 05.js
@@ -0,0 +1,21 @@
+function fnLoad() {
+  document.head.outerHTML='';
+  document.body.innerHTML='';
+  var eFrm = document.createElement('iframe');
+  eFrm.src = 'https://www.airkorea.or.kr/web/vicinityStation?item_code=10008&station_code=336112';
+  eFrm.style = 'width: 570px;height: 422px;';
+  document.body.appendChild(eFrm);
+  var eFrm = document.createElement('iframe');
+  eFrm.src = 'https://www.airkorea.or.kr/web/vicinityStation?item_code=10007&station_code=336112';
+  eFrm.style = 'width: 570px;height: 422px;';
+  document.body.appendChild(eFrm);
+  var eImg = document.createElement('img');
+  eImg.src = 'https://www.airkorea.or.kr/web/placeInfo/getImgFile?scrinId=16600&amp;Dx=D0&amp;imageSn=3';
+  eImg.style = 'width: 492px; padding-top: 12px;';
+  eImg.alt = "초미세먼지 공간분포 이미지";
+  document.body.appendChild(eImg);
+  eImg.src = 'https://www.airkorea.or.kr/web/placeInfo/getImgFile?scrinId=16600&amp;Dx=D0&amp;imageSn=1';
+  eImg.style = 'width: 492px; padding-top: 12px;';
+  eImg.alt = "미세먼지 공간분포 이미지";
+  document.body.appendChild(eImg);
+}
